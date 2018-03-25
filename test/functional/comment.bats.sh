6e6906c4772b154ff0f0107e037e603c6eccd9fc
kstenschke <info@stenschke.com>
2018-03-25 18:00:06 +0200

    Added funtional tests for multiple ids used w/ comment command

--- test/functional/comment.bats.sh
+++ test/functional/comment.bats.sh
@@ -35,3 +35,69 @@ load test_helper
   run grep -c 'foo' $BATS_TEST_DIRNAME/timesheet.html
   [[ "$output" = 0 ]]
 }
+
+@test 'Multiple items comments can be set by passing comma-separated indexes' {
+  # Create 6 entries commented w/ "foo"
+  $BATS_TEST_DIRNAME/tsp s
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s
+  $BATS_TEST_DIRNAME/tsp p
+
+  # There are 6 entries now
+  run grep -c '<td class="meta">' $BATS_TEST_DIRNAME/timesheet.html
+  [[ "$output" = 6 ]]
+  # There are 0 "foo" comments
+  amount_foos=$(cat $BATS_TEST_DIRNAME/timesheet.html | grep "foo" -o | wc -l | xargs)
+  [[ "$amount_foos" = 0 ]]
+
+  # Remove multiple comments
+  run $BATS_TEST_DIRNAME/tsp c i=1,3,5 foo
+
+  # There are 6 entries now
+  run grep -c '<td class="meta">' $BATS_TEST_DIRNAME/timesheet.html
+  [[ "$output" = 6 ]]
+  # There are 3 "foo" comments
+  amount_entries=$(cat $BATS_TEST_DIRNAME/timesheet.html | grep "foo" -o | wc -l | xargs)
+  [[ "$amount_entries" = 3 ]]
+}
+
+@test 'Multiple items comments can be removed by passing comma-separated indexes' {
+  # Create 6 entries commented w/ "foo"
+  $BATS_TEST_DIRNAME/tsp s foo
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s foo
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s foo
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s foo
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s foo
+  $BATS_TEST_DIRNAME/tsp p
+  $BATS_TEST_DIRNAME/tsp s foo
+  $BATS_TEST_DIRNAME/tsp p
+
+  # There are 6 entries now
+  run grep -c '<td class="meta">' $BATS_TEST_DIRNAME/timesheet.html
+  [[ "$output" = 6 ]]
+  # There are 6 "foo" comments
+  amount_foos=$(cat $BATS_TEST_DIRNAME/timesheet.html | grep "foo" -o | wc -l | xargs)
+  [[ "$amount_foos" = 6 ]]
+
+  # Remove multiple comments
+  run $BATS_TEST_DIRNAME/tsp c i=1,3,5
+
+  # There are 6 entries now
+  run grep -c '<td class="meta">' $BATS_TEST_DIRNAME/timesheet.html
+  [[ "$output" = 6 ]]
+  # There are 3 "foo" comments
+  amount_entries=$(cat $BATS_TEST_DIRNAME/timesheet.html | grep "foo" -o | wc -l | xargs)
+  [[ "$amount_entries" = 3 ]]
+}
