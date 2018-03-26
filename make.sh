85c1069474d2622a388eaa91347ce2e1d25e3910
kstenschke <info@stenschke.com>
2018-03-26 21:31:56 +0200

    improve cmake config

--- make.sh
+++ make.sh
@@ -5,18 +5,3 @@ cmake CMakeLists.txt
 
 # Compile, link
 make
-
-# Move into bin/<CURRENT_OS> directory
-case "$OSTYPE" in
-  bsd*)     OS="bsd" ;;
-  darwin*)  OS="osx" ;;
-  linux*)   OS="linux" ;;
-  solaris*) OS="solaris" ;;
-  *)        OS="unknown: $OSTYPE" ;;
-esac
-
-if [ "$OS" == "osx" ]; then
-  mv ./tsp bin/mac/tsp
-elif [ $OS == "linux" ]; then
-  mv ./tsp bin/linux/tsp
-fi
